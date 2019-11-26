64a919ee30d9d0cf2014a992d0535b5a0443072c
c00305146 <lauren.walling1@louisiana.edu>
2019-11-25 23:35:36 -0600

    #addition of comments

--- R-project.R
+++ R-project.R
@@ -1,1 +1,33 @@
 #R-project
+#URL for github repository= https://github.com/c00305146/R-project
+#reading in the data
+library(tidyverse)
+read.csv("all_samples.csv")
+A= All Samples.csv
+#Application of the appropriate data storage structure: list, data frame, matrix or array (2 points) 
+#Example of indexing (2 points) 
+#Subsetting (2 points) 
+#subset out range#####
+#Ordering (2 points) 
+#order from depth#####
+#Summarizing (5 points) 
+#use to take the average depth y methylation######
+#Merge or Join data frames (5 points) 
+#Custom function(s) (10 points) 
+#make function to make dark, light or shallow#####
+#Custom operator(s) (10 points) 
+#‘if else’ statement (10 points)
+#if else statement will be in function######
+#Reshaping data with ‘melt’ and/or ‘dcast’ (5 points) 
+#‘for loop’ (10 points) 
+#use funtion to for loop through data#####
+# ‘ddply’ (10 points) 
+#use ddply to clean up 1st and 2nd plates#####
+#Histogram plot (5 points)
+#make a histogram of the %methylation####
+#Point, bar, or line plot (whichever makes the most sense) (5 points) 
+#‘ggplot’ with at least 2 geoms (e.g. point, bar, tile), use one of the ‘scale_’ geoms, and adjusting the theme of the plot (10 points)
+# A map of showing the geographic location where the data was collected (10 points)
+#use code that is already made for this######
+#Exporting data set (2 points) 
+#Exporting and saving figures from ggplot (2 points) 
