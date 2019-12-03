0a67f033ff45d60634a2321ef1bc779d171b667f
c00305146 <lauren.walling1@louisiana.edu>
2019-12-03 14:40:49 -0600

    Additon of range and merge, saving files, ggplots, and map

--- R-project.R
+++ R-project.R
@@ -14,29 +14,31 @@ A[,4]
 #indexing depth of collections
 A[,6]
 
-#Subsetting (2 points) -
-litsamples=A[A$Range=="light",]
-darksamples=A[A$Range=="dark",]
-shallowsamples=A[A$Range=="shallow",]
-
 #Ordering (2 points)- 
 #order from increasing depth
 B= arrange(A, Depth)
 #order from increaseing methylation
 C= arrange(A, Methylation)
-#Summarizing (5 points) 
+
+#Summarizing (5 points)-
 library(tidyverse)
 #average depth and methylation
 D=summarise(A, meanmeth=mean(Methylation),meandepth=mean(Depth, na.rm = T))
-#range of depth and methylation________________
-E=summarise(A, rangemeth=range(Methylation))
-summarise
-#Merge or Join data frames (5 points) 
-#join the two summarise together so they are in one data frame#######
+
+#Range of Methylation and Depth
+methrange=range(A$Methylation)
+methrange=as.data.frame(methrange)
+methrange$seq=seq(1,nrow(methrange),1)
+depthrange=range(A$Depth)
+depthrange=as.data.frame(depthrange)
+depthrange$seq=seq(1,nrow(depthrange),1)
+#Merge ranges of methylation and depth
+E=merge(x=depthrange,y=methrange,by="seq",no.dups=T,all.x=T)
+
 #Custom function(s) (10 points) -
 #‘if else’ statement (10 points)-
 #‘for loop’ (10 points) -
-#make function to make dark, light or shallow
+#function to make dark, light or shallow in Range colomn
 A$Range=NA
 Rangefunc=function(x){
   if(x>0& x<=200.0)
@@ -51,13 +53,54 @@ for(i in 1:nrow(A)){
   A[i,]$Range<-Rangefunc(x=A[i,]$Depth)
 }
 A$Range
-#Custom operator(s) (10 points) 
-# ‘ddply’ (10 points) 
-#Histogram plot (5 points)
-#make a histogram of the %methylation####
-#Point, bar, or line plot (whichever makes the most sense) (5 points) 
-#‘ggplot’ with at least 2 geoms (e.g. point, bar, tile), use one of the ‘scale_’ geoms, and adjusting the theme of the plot (10 points)
-# A map of showing the geographic location where the data was collected (10 points)
-#use code that is already made for this######
+
 #Exporting data set (2 points) 
-#Exporting and saving figures from ggplot (2 points) 
+save(A, file="all_samples_range.csv")
+
+#Histogram plot (5 points)-
+#histogram of the %methylation
+library(ggplot2)
+ggplot(data = A,aes(x=Methylation))+
+  geom_histogram(binwidth = 0.3)
+#histogram of the depth
+ggplot(data = A,aes(x=Depth))+
+  geom_histogram(binwidth =200.0)
+
+#Point, bar, or line plot (whichever makes the most sense) (5 points)-
+#point graph of methylation vs. depth
+ggplot(data = A,aes(x=Depth,y=Methylation))+
+  geom_point()
+
+#‘ggplot’ with at least 2 geoms (e.g. point, bar, tile), use one of the ‘scale_’ geoms, and adjusting the theme of the plot (10 points)-
+plot=ggplot(A)+ 
+  geom_point(data=A,aes(x=Depth, y=Methylation))+
+  geom_rect(data=A, aes(xmin=Start,xmax=End,fill=Range),
+            ymin=-Inf,ymax=Inf,alpha=0.04)+
+  scale_fill_manual(values = c("firebrick","brown2","orange"))+
+  theme_classic()+
+  ggtitle(label = "Depth vs %Methylation")
+
+#Exporting and saving figures from ggplot (2 points)
+ggsave(filename =("Depth vs Methylation.tiff"),
+       plot = plot, width = 4, height = 3, units = 'in',
+       dpi = 300)
+
+# A map of showing the geographic location where the data was collected (10 points)-
+library(rgdal)
+library(sp)
+library(maptools)
+library(tidyverse)
+library(osmdata)
+library(ggmap)
+#Subset Depths --------------------------------------------------------------
+shallow=A[A$Range=="shallow",]
+light=A[A$Range=="light",]
+dark=A[A$Range=="dark",]
+# Plot Points by Depth -------------------------------------------------------------
+data(wrld_simpl)
+plot(wrld_simpl, xlim=c(-100,100), ylim=c(-100,100), axes=TRUE, col="gray")
+box()
+
+points(shallow$Long, shallow$Lat, col='blue', pch=20, cex=1.5)
+points(light$Long, light$Lat, col='green', pch=20, cex=1.5)
+points(dark$Long, dark$Lat, col='red3', pch=20, cex=1.5)
